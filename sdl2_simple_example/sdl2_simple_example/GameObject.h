69839c4366bbb86fbfb27f7d6519898d4bf729a0
Lexi <119694326+Apb8@users.noreply.github.com>
2024-12-24 02:21:40 +0100

    Transformaciones des del Inspector funcionando ole

--- sdl2_simple_example/sdl2_simple_example/GameObject.h
+++ sdl2_simple_example/sdl2_simple_example/GameObject.h
@@ -5,9 +5,9 @@
 #include <vector>
 #include <memory>
 #include "Component.h"
+#include "ComponentTransform.h"
 #include <glm/gtc/type_ptr.hpp> //para el render
 
-class ComponentTransform; //declaracion anticipada ?? para los errores pero luego me lo miro
 
 class GameObject {
 public:
@@ -35,7 +35,7 @@ public:
 
         glm::mat4 transform = glm::mat4(1.0f);
 
-        // Buscar el componente de transformación
+        // buscar el componente de transformacion
         for (auto& component : components) {
             if (component && component->GetType() == Component::Type::Transform) {
                 auto* transformComponent = dynamic_cast<ComponentTransform*>(component.get());
@@ -46,16 +46,15 @@ public:
             }
         }
 
-        // Aplicar la transformación en OpenGL
+        // aplicar transformacion en OpenGL
         glPushMatrix();
         glMultMatrixf(glm::value_ptr(transform));
-
-        // Renderizar todos los componentes
+                
         for (auto& component : components) {
             if (component) component->Render();
         }
 
-        glPopMatrix(); // Restaurar la matriz
+        glPopMatrix();
     }
 
     void SetActive(bool active) { this->active = active; }
