475748041508373cadf878183b9c16beee615d32
Lexi <119694326+Apb8@users.noreply.github.com>
2024-12-31 12:58:43 +0100

    Revert "Separated Camera from main to independent module"
    This reverts commit 8bd8869713f217b412f6b728b35f1b9c8f58ddfd.

--- sdl2_simple_example/sdl2_simple_example/main.cpp
+++ sdl2_simple_example/sdl2_simple_example/main.cpp
@@ -13,7 +13,6 @@
 #include "Scene.h"
 #include "GameObject.h"
 #include "ComponentMesh.h"
-#include "Camera.h"
 
 #include <stdio.h>
 #include <assimp/cimport.h>
@@ -46,8 +45,34 @@ static const ivec2 WINDOW_SIZE(1200, 600); //1300 800 para poder trabajr en el p
 static const unsigned int FPS = 60;
 static const auto FRAME_DT = 1.0s / FPS;
 
+float camera_angle = 0.0f; // angulo d camara
+const float camera_radius = 10.0f; // distancia de la camara al origen
+
+float camX = sin(camera_angle) * camera_radius;
+float camZ = cos(camera_angle) * camera_radius;
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 5.0f, 8.0f);
+glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 cameraRight = glm::vec3(1.0f, 0.0f, 0.0f);
+glm::vec3 cameraFront = -cameraDirection;
+
+float yaw = glm::degrees(atan2(cameraFront.z, cameraFront.x));
+float initialyaw = glm::degrees(atan2(cameraFront.z, cameraFront.x));
+float pitch = glm::degrees(asin(cameraFront.y));
+float initialpitch = glm::degrees(asin(cameraFront.y));
+bool isLeftButtonPressed = false;
+bool isRightButtonPressed = false;
+int lastMouseX, lastMouseY;
+
+float fov = 45.0f;
 bool houseLoaded = false;
 bool rotation = false;
+
+glm::mat4 projectionMatrix;
+glm::mat4 viewMatrix;
 
 static void init_openGL() {
 
@@ -62,6 +87,151 @@ static void init_openGL() {
     glMatrixMode(GL_MODELVIEW);
 }  
 
+void updateCamera() {
+
+    const float sensitivity = 0.1f;
+    const float cameraSpeed = 0.05f;
+
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+
+    int mouseX, mouseY;
+	float xOffset, yOffset;
+    Uint32 mouseState = SDL_GetMouseState(&mouseX, &mouseY);
+
+    if (mouseState & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
+
+        if (!isRightButtonPressed) {
+            lastMouseX = mouseX;
+            lastMouseY = mouseY;
+            isRightButtonPressed = true;
+            rotation = false;
+        }
+
+        xOffset = mouseX - lastMouseX;
+        yOffset = lastMouseY - mouseY;//Invertimos la y
+
+        xOffset *= sensitivity;
+        yOffset *= sensitivity;
+
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
+
+        yaw += xOffset;
+        pitch += yOffset;
+
+        if (pitch > 89.0f)
+            pitch = 89.0f;
+        if (pitch < -89.0f)
+            pitch = -89.0f;
+
+        glm::vec3 newcameraDirection;
+        newcameraDirection.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+        newcameraDirection.y = sin(glm::radians(pitch));
+        newcameraDirection.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+        cameraFront = glm::normalize(newcameraDirection);
+
+        if (state[SDL_SCANCODE_W]) {
+
+            cameraPos += cameraSpeed * cameraFront;
+            if (state[SDL_SCANCODE_LSHIFT]) {
+
+                cameraPos += 2 * cameraSpeed * cameraFront;
+            }
+        }
+        if (state[SDL_SCANCODE_S]) {
+
+            cameraPos -= cameraSpeed * cameraFront;
+            if (state[SDL_SCANCODE_LSHIFT])
+            {
+                cameraPos -= 2 * cameraSpeed * cameraFront;
+            }
+        }
+
+        if (state[SDL_SCANCODE_A]) {
+
+            cameraPos -= cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+
+            if (state[SDL_SCANCODE_LSHIFT])
+            {
+                cameraPos -= 2 * cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+            }
+        }
+        if (state[SDL_SCANCODE_D]) {
+
+            cameraPos += cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+
+            if (state[SDL_SCANCODE_LSHIFT])
+            {
+                cameraPos += 2 * cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+            }
+        }
+
+        if (state[SDL_SCANCODE_Q]) { //Añadir zoom
+                fov -= 1.0f;
+        }
+        else if (state[SDL_SCANCODE_E]) { //Quitar zoom
+            fov += 1.0f;
+        }
+        if (fov < 1.0f)
+            fov = 1.0f;
+        if (fov > 45.0f)
+            fov = 45.0f;
+
+        //Actualiza la perspectiva con el nuevo FOV
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(fov, (double)WINDOW_SIZE.x / WINDOW_SIZE.y, 0.1, 100.0);
+        glMatrixMode(GL_MODELVIEW);
+    }
+    else if (mouseState & SDL_BUTTON(SDL_BUTTON_LEFT))
+    {
+        if (!isLeftButtonPressed) {
+
+            isLeftButtonPressed = true;
+        }
+        if (state[SDL_SCANCODE_LALT]) {
+
+            if (rotation == false)
+            {
+                std::cout << "Camera rotation enabled" << std::endl;
+                rotation = true;
+			}
+            else
+            {
+                std::cout << "Camera rotation stopped" << std::endl;
+                rotation = false;
+            }
+        }
+    }
+    else {
+
+		isLeftButtonPressed = false;
+        isRightButtonPressed = false;
+    }
+
+    //Centrar mirada al objeto
+    if (state[SDL_SCANCODE_F]) {
+        std::cout << "Camera centered" << std::endl;
+        cameraFront = glm::normalize(glm::vec3(0.0f, 0.0f, 0.0f) - cameraPos);
+		yaw = initialyaw;
+		pitch = initialpitch;
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
+    }
+
+    if (rotation == true) {
+        camera_angle += 0.005f;
+        if (camera_angle > 2.0f * 3.14159265f) camera_angle = 0.0f;
+        cameraPos.x = sin(camera_angle) * camera_radius;
+        cameraPos.z = cos(camera_angle) * camera_radius;
+        cameraFront = glm::normalize(glm::vec3(0.0f, 0.0f, 0.0f) - cameraPos);
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
+    }
+
+    projectionMatrix = glm::perspective(glm::radians(fov), (float)WINDOW_SIZE.x / (float)WINDOW_SIZE.y, 0.1f, 100.0f); //quizas mover de lugar
+
+}
 static bool processEvents() {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -86,30 +256,21 @@ int main(int argc, char** argv) {
     ilInit();
     init_openGL();
 
-    //instancia de Camera
-    Camera camera(glm::vec3(0.0f, 5.0f, 8.0f), glm::vec3(0.0f, 0.0f, 0.0f), 45.0f,
-        (float)WINDOW_SIZE.x / (float)WINDOW_SIZE.y, 0.1f, 100.0f);
+    projectionMatrix = glm::perspective(glm::radians(fov), (float)WINDOW_SIZE.x / (float)WINDOW_SIZE.y, 0.1f, 100.0f); //mousepicking
 
     while (window.processEvents() && window.isOpen()) {
 
         const auto t0 = hrclock::now();
         if (!window.IsPaused()) {
-            //actualizar camara
-            const Uint8* keyboardState = SDL_GetKeyboardState(NULL);
-            int mouseX, mouseY;
-            Uint32 mouseState = SDL_GetMouseState(&mouseX, &mouseY);
-            camera.Update(FRAME_DT.count(), keyboardState, mouseX, mouseY, mouseState);
-                        
+            updateCamera();
             scene.Update();
         }
-        //renderizar
+        viewMatrix = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);//mousepicking
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
-        gluLookAt(camera.GetPosition().x, camera.GetPosition().y, camera.GetPosition().z,
-                  camera.GetPosition().x + camera.GetViewMatrix()[2][0],
-                  camera.GetPosition().y + camera.GetViewMatrix()[2][1],
-                  camera.GetPosition().z + camera.GetViewMatrix()[2][2],
-                  0.0f, 1.0f, 0.0f);
+        gluLookAt(cameraPos.x, cameraPos.y, cameraPos.z,
+            cameraPos.x + cameraFront.x, cameraPos.y + cameraFront.y, cameraPos.z + cameraFront.z,
+            cameraUp.x, cameraUp.y, cameraUp.z);
 
         scene.DrawGrid();
         scene.Render();
@@ -126,8 +287,8 @@ int main(int argc, char** argv) {
                 int mouseX, mouseY;
                 SDL_GetMouseState(&mouseX, &mouseY);
 
-                glm::vec3 rayDir = camera.CalculateRay(mouseX, mouseY, WINDOW_SIZE.x, WINDOW_SIZE.y);
-                glm::vec3 rayOrigin = camera.GetPosition();
+                glm::vec3 rayDir = CalculateRay(mouseX, mouseY, WINDOW_SIZE.x, WINDOW_SIZE.y, projectionMatrix, viewMatrix);
+                glm::vec3 rayOrigin = cameraPos;
 
                 auto selectedObject = scene.PickGameObject(rayOrigin, rayDir);
 
